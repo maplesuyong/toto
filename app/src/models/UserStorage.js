116e83c555772993d55f1d6ea32b409fdaa3225c
maplesuyong <suyong0507@gmail.com>
2021-11-29 00:30:46 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,6 +17,25 @@ class UserStorage {
         }, {});
         return newUsers;
     }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.pw.push(userInfo.pw);
+        return { success: true };
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -16,22 +16,13 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const pw = req.body.pw;
-
-        const users = UserStorage.getUsers("id", "pw");
-
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.pw[idx] === pw) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response);
+    },
+    register: (req, res) => {
+        const user = new User(req.body);
+        const response = user.register();
         return res.json(response);
     },
 };
