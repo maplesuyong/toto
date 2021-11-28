a24e80a26a930d1a5ea0fa016ab2cc9c7dd8c246
maplesuyong <suyong0507@gmail.com>
2021-11-28 20:13:52 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 // 미들웨어 등록
 app.use("/", home);
