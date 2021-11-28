ec984e5f8cb4cbc448cdcc29a4064889d53af4f0
maplesuyong <suyong0507@gmail.com>
2021-11-28 23:34:54 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,22 @@
 "use strict";
 
 const id = document.querySelector("#id");
+const name = document.querySelector("#name");
 const pw = document.querySelector("#pw");
-const loginBtn = document.querySelector("#button");
+const confirmPw = document.querySelector("#confirm-pw");
+const registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         pw: pw.value,
+        confirmPw: confirmPw.value,
     };
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -21,12 +25,12 @@ function login() {
     }).then((res) => res.json())
     .then((res) => {
         if(res.success) {
-            location.href = "/";
+            location.href = "/login";
         } else {
             alert(res.msg);
         }
     })
     .catch((err) => {
-        console.error("로그인 중 에러 발생");
+        console.error("회원가입 중 에러 발생");
     });
 }
