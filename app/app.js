c15a728084aa4f29c3b83a7f20af8326392148b0
maplesuyong <suyong0507@gmail.com>
2021-11-28 20:04:34 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT  = 3000;
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+// 미들웨어 등록
+app.use("/", home);
+
+module.exports = app;
+
+
+
+
---
+++ app/bin/www.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const app = require("../app");
+const PORT  = 3000;
+
+app.listen(PORT, () => {
+    console.log("start! express server on port 3000");
+});
